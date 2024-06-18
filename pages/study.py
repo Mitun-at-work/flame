e766c4940a97e066c99ca0c7b97ac7e72f809441
mitun-at-work <mitun.focused@gmail.com>
2024-06-18 21:55:25 +0530

    Refactor code: remove unused imports and delete directories in study.py

--- pages/study.py
+++ pages/study.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from os import listdir
+from os import listdir, removedirs
 from constants.const import case_study_folder
 from model.case_study_model import CaseStudyModel
 
@@ -10,7 +10,6 @@ class Study:
     def __init__(self) -> None:
         # Initialising Folder Path
         self.case_folder_path = '.config/case'
-
         self.case_folder = listdir(self.case_folder_path)
 
     def get_content(self, folder_link : str) -> CaseStudyModel:
@@ -38,6 +37,9 @@ class Study:
         )
 
         return model
+    
+    def ren(self, file_link) :
+        print(file_link)
 
     def render_view(self) :
         # Title of the Page
@@ -55,21 +57,23 @@ class Study:
 
         # Case File Link
         case_files_link = f"{self.case_folder_path}/{folder}"
+        
+        if st.button('Delete') :
+            for i in case_files_link :
+                removedirs(f'{case_files_link}/{i}')
 
         # Folders with Random ID
         files = listdir(case_files_link)
 
         # Generate View
         for folder in listdir(f'{case_files_link}') :
+
             case_study = self.get_content(f"{case_files_link}/{folder}")
             case_study_container = st.container(border= True)
-
             case_study_container.title(case_study.title)
-
             case_study_container.caption(f'Created On {case_study.date} \n {case_study.time}')
             case_study_container.image(case_study.image_byte)
             case_study_container.subheader(case_study.notes)
-           
 
 if __name__ == '__main__' :
     Study().render_view()
